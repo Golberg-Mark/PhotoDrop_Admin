3cb76a30283d3ab944947f2971a9920c93a4cb90
Mark_bitbucket <golbergmark@gmail.com>
2022-09-05 17:22:04 +0300

    Removed unused lib

--- src/store/actions/common.ts
+++ src/store/actions/common.ts
@@ -1,8 +1,5 @@
 import { ThunkAction } from 'redux-thunk';
 
 import { State, api, Actions } from '@/store';
-import { CallHistoryMethodAction } from 'connected-react-router';
 
-type HistoryActions = CallHistoryMethodAction<[string, unknown?]> | CallHistoryMethodAction<[]>;
-
-export type AsyncAction<R = void> = ThunkAction<R, State, typeof api, Actions | HistoryActions>;
+export type AsyncAction<R = void> = ThunkAction<R, State, typeof api, Actions>;
