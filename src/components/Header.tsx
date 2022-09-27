b593da10ea50255523eff59ca9c18d3331eda642
Mark_bitbucket <golbergmark@gmail.com>
2022-09-28 00:10:37 +0300

    Fixed add albums button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Logo from '@/icons/Logo';
 import { useSelector } from 'react-redux';
-import { selectIsHeaderVisible } from '@/store/selectors/userSelector';
+import { selectAlbums, selectIsHeaderVisible } from '@/store/selectors/userSelector';
+import { useLocation } from 'react-router';
+import useToggle from '@/hooks/useToggle';
+import CreateAlbum from '@/components/CreateAlbum';
 
 const Header = () => {
+  const [isCreateModalVisible, toggleIsCreateModalVisible] = useToggle();
+  const { pathname } = useLocation();
   const isVisible = useSelector(selectIsHeaderVisible);
+  const albums = useSelector(selectAlbums);
+
+  useEffect(() => {
+    toggleIsCreateModalVisible(false);
+  }, [albums]);
 
   return (
     <Container isVisible={isVisible}>
       <Logo />
+      {pathname === '/' ? (
+        <CreateAlbumButton>
+          <PlusButton onClick={toggleIsCreateModalVisible}>
+            +
+          </PlusButton>
+        </CreateAlbumButton>
+      ) : ''}
+      {isCreateModalVisible ? <CreateAlbum hide={toggleIsCreateModalVisible} /> : null}
     </Container>
   );
 };
@@ -24,6 +42,35 @@ const Container = styled.header<{ isVisible: boolean }>`
   
   div {
     margin: 0 auto;
+  }
+`;
+
+const CreateAlbumButton = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 40px;
+`;
+
+const PlusButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 50%;
+  background-color: #3300CC;
+  font-size: 30px;
+  font-weight: 500;
+  color: #fff;
+  cursor: pointer;
+  
+  :focus {
+    outline: none;
+  }
+  
+  :hover {
+    opacity: .7;
   }
 `;
 
--- src/containers/Albums.tsx
+++ src/containers/Albums.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 
 import { getAlbumsAction, userActions } from '@/store/actions/userActions';
 import { selectAlbums } from '@/store/selectors/userSelector';
 import AlbumItem from '@/components/Album/AlbumItem';
 import Loader from '@/components/Loader';
 import PageWrapper from '@/components/PageWrapper';
-import useToggle from '@/hooks/useToggle';
-import CreateAlbum from '@/components/CreateAlbum';
 
 interface Props {
   isLoggedIn: boolean
@@ -18,7 +15,6 @@ interface Props {
 const Albums: React.FC<Props> = ({ isLoggedIn }) => {
   if (!isLoggedIn) return <Navigate to="/auth" replace />;
 
-  const [isCreateModalVisible, toggleIsCreateModalVisible] = useToggle();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const albums = useSelector(selectAlbums);
@@ -38,64 +34,23 @@ const Albums: React.FC<Props> = ({ isLoggedIn }) => {
     }
   }, []);
 
-  useEffect(() => {
-    if (isCreateModalVisible) toggleIsCreateModalVisible(false);
-  }, [albums]);
-
   return (
-    <>
-      <PageWrapper>
-        {albums?.length ? (
-          <>
-            <CreateAlbumButton>
-              <PlusButton onClick={toggleIsCreateModalVisible}>
-                +
-              </PlusButton>
-            </CreateAlbumButton>
-            {albums.map((el, i) => (
-              <AlbumItem
-                key={`${el.name}_${i}`}
-                name={el.name}
-                location={el.location}
-                date={el.date}
-                onClick={() => onAlbumClick(el.name)}
-              />
-            ))}
-          </>
-        ) : <Loader />}
-      </PageWrapper>
-      {isCreateModalVisible ? <CreateAlbum hide={toggleIsCreateModalVisible} /> : null}
-    </>
+    <PageWrapper>
+      {albums?.length ? (
+        <>
+          {albums.map((el, i) => (
+            <AlbumItem
+              key={`${el.name}_${i}`}
+              name={el.name}
+              location={el.location}
+              date={el.date}
+              onClick={() => onAlbumClick(el.name)}
+            />
+          ))}
+        </>
+      ) : <Loader />}
+    </PageWrapper>
   );
 };
-
-const CreateAlbumButton = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 40px;
-`;
-
-const PlusButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  border: none;
-  border-radius: 50%;
-  background-color: #3300CC;
-  font-size: 30px;
-  font-weight: 500;
-  color: #fff;
-  cursor: pointer;
-  
-  :focus {
-    outline: none;
-  }
-  
-  :hover {
-    opacity: .7;
-  }
-`;
 
 export default Albums;
